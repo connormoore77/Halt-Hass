485f122c27b69bd12d238670ec78280630db7a56
NGrieser <nathan.a.grieser-1@ou.edu>
2017-03-17 09:13:09 -0500

    Three switch control
    The arduino code to be added to the MCU for use of three different valves.

---
+++ AirRegulation/varFreq/Mosfet_Control1.ino
@@ -0,0 +1,36 @@
+#define O0 11
+#define O1 10
+#define O2 9
+#define O3 6
+#define O4 5
+int deltaT=80;
+
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+pinMode(O1,OUTPUT);
+pinMode(O2,OUTPUT);
+pinMode(03,OUTPUT);
+
+}
+
+void loop() {
+
+  digitalWrite(O1,HIGH);
+  delay(deltaT/6);
+  digitalWrite(O1,LOW);
+  digitalWrite(02,HIGH);
+  delay(deltaT/6);
+  digitalWrite(02,LOW);
+  digitalWrite(03,HIGH);
+  delay(deltaT/6);
+  digitalWrite(O3,LOW);
+  delay(deltaT/6);
+  
+   if (Serial.available()>0) 
+   {
+        deltaT = (Serial.readString()).toInt();
+      delay(100);
+   }
+
+}
